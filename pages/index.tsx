2b42a6f289f4931a0a21e4f9810ff978c9f18dcd
Jari Kasandiredjo <jarikasan@gmail.com>
2022-07-24 15:25:39 +0200

    rm semicolons from imports

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next'
-import Head from 'next/head';
-import Link from 'next/link';
+import Head from 'next/head'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {

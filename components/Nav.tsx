2521d0ecba00be317019532353f83f99586cbb0e
Jari Kasandiredjo <jarikasan@gmail.com>
2022-07-24 17:21:39 +0200

    rm semicolon

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import styles from '../styles/Nav.module.css'
 

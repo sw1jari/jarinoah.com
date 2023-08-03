52aa4669e5f59efc12ac1a713d10bbad6b119155
Jari Kasandiredjo <jarikasan@gmail.com>
2023-08-03 21:10:26 +0200

    remove penang hill from code as well

--- lib/photos.ts
+++ lib/photos.ts
@@ -1,9 +1,7 @@
 import strand3 from '../public/photos/strand-3.webp'
-import penangHill039 from '../public/photos/penang_hill039.webp'
 
 const photos = [
   strand3, 
-  penangHill039,
 ]
 
 export default photos
